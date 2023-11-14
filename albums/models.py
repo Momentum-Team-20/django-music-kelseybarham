786c1507ffeabaef43577e73dee42083adb16910
Kelsey Barham <kelseylbarham@gmail.com>
2023-11-14 09:06:58 -0500

    created superuser, registration profiles shows in django admin page, added base.html to users/templates

--- albums/models.py
+++ albums/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+
+
+class Album(models.Model):
+    album_title = models.CharField(max_length=200)
+    artist_name = models.CharField(max_length=200)
+    created_at = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -125,3 +126,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+LOGIN_REDIRECT_URL = '/'
