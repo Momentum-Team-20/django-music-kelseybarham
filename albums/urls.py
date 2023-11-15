486f5759a17de94bdc9d17a9886f4b2f94b7d394
Kelsey Barham <kelseylbarham@gmail.com>
2023-11-15 16:11:51 -0500

    can edit and delete albums now, woohoo

--- albums/urls.py
+++ albums/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.list_albums, name='home'),
     path('album_detail/<int:pk>', views.album_detail, name='album-detail'),
     path('create_album', views.create_new_album, name='create-album'),
+    path('album/<int:pk>/edit/', views.edit_album, name='edit-album'),
+    path('delete_album/<int:pk>', views.delete_album, name="delete-album")
 ]
--- albums/views.py
+++ albums/views.py
@@ -22,3 +22,20 @@ def create_new_album(request):
     else:
         form = AlbumForm()
     return render(request, 'albums/new_album.html', {'form': form})
+
+def edit_album(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+
+    if request.method == 'POST':
+        form = AlbumForm(request.POST, instance=album)
+        if form.is_valid():
+            form.save()
+            return redirect('album_detail', pk=pk)
+    else:
+        form = AlbumForm(instance=album)
+    return render(request, 'albums/edit_album.html', {'form': form})
+
+def delete_album(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+    album.delete()
+    return redirect('home')
