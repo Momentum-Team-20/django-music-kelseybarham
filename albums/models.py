cd7257e642a2e76ecc2da522d8fd1ff402d29bad
Kelsey Barham <kelseylbarham@gmail.com>
2023-11-15 08:44:02 -0500

    populated albums in django admin

--- albums/models.py
+++ albums/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Album(models.Model):
-    album_title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     artist_name = models.CharField(max_length=200)
-    created_at = models.DateField(null=True, blank=True)
+    created_at = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.title
