293e1ca141f454cfdac846a00eedba30277e6681
Kelsey Barham <kelseylbarham@gmail.com>
2023-11-15 10:09:48 -0500

    individual albums are populating on the webpage

--- albums/urls.py
+++ albums/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list_albums, name='home')
+    path('', views.list_albums, name='home'),
+    path('album_detail/<int:pk>', views.album_detail, name='album-details')
 ]
--- albums/views.py
+++ albums/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Album
+
 
 # Create your views here.
 def list_albums(request):
     albums = Album.objects.all()
     return render(request, 'albums/index.html', {'albums': albums})
+
+def album_detail(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+    return render(request, 'albums/album_detail.html', context={'album': album})
+
